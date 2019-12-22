9a46b6ee1621c11053acc15bcec9a1585d8dcea6
Sergey <darkus007@yandex.ru>
2019-12-22 22:30:19 +0300

    The seventh and eighth sections are completed.

--- src/js/script.js
+++ src/js/script.js
@@ -3,5 +3,16 @@ $(document).ready(function(){
         $(this)
           .addClass('tabs__btn_active').siblings().removeClass('tabs__btn_active')
           .closest('div.container').find('div.tabs__item').removeClass('tabs__item_active').eq($(this).index()).addClass('tabs__item_active');
-      });
+    });
+
+    // изменение элемента при наведении на другой элемент
+    $('.pricing__item').hover(function() {
+        // навели мышь на элемент
+        $(this).children('.pricing__subdescr').css('height', '39px');
+        $(this).css('transform', 'translateY(-34px)');
+    }, function() {
+        // убрали мышь с элемента
+        $(this).children('.pricing__subdescr').css('height', '5px');
+        $(this).css('transform', 'translateY(0px)');
+    });
 });
