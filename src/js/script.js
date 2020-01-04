ded7eb8ad4616cdb964ea8d754844c984bfeb61c
Sergey <darkus007@yandex.ru>
2020-01-04 20:35:00 +0300

    Added optimization for various resolutions (media).

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,21 @@ $(document).ready(function(){
         $(this).css('transform', 'translateY(0px)');
     });
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+});
